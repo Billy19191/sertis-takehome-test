2a0a0de253fa079c441c79cb38f2cd7e602ad5f6
Billy19191 <bill.ratchanon@gmail.com>
2025-03-19 23:27:43 +0700

    ⚡️  feat: landing page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,15 @@
 const HomePage = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-black">
-      <h1 className="text-3xl font-bold text-primary-500 font-poppins">
-        Sertis
-      </h1>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-[url('/background/BG_Welcome.png')] bg-cover bg-center">
+      <div className="text-[40px] font-bold text-white font-poppins">
+        We have
+      </div>
+      <div className="text-[96px] font-bold text-white font-poppins">
+        A Special Deal
+      </div>
+      <div className="text-[72px] font-bold text-white font-old-standard-tT italic">
+        For you
+      </div>
     </div>
   )
 }
